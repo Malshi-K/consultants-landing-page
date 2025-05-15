0c562abb035d9c9f49b0070d04a05265881a7ef1
Malshii <kulasinghe1malshi@gmail.com>
2025-05-15 10:45:36 +0530

    chatbot added

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+"use client";
+import { useEffect } from "react";
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -12,12 +14,37 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
-export const metadata = {
-  title: "GDC Consultants",
-  description: "GDC Consultants | Engineering Solutions",
-};
-
 export default function RootLayout({ children }) {
+  useEffect(() => {
+    const trackingScriptId = "hs-tracking-code";
+    if (!document.getElementById(trackingScriptId)) {
+      const trackingScript = document.createElement("script");
+      trackingScript.src = `https://js.hs-scripts.com/6187835.js`;
+      trackingScript.id = trackingScriptId;
+      trackingScript.async = true;
+      trackingScript.defer = true;
+      document.head.appendChild(trackingScript);
+    }
+
+    const chatbotScriptId = "hs-chatbot-loader";
+    if (!document.getElementById(chatbotScriptId)) {
+      const chatbotScript = document.createElement("script");
+      chatbotScript.src = `https://js.hs-scripts.com/6187835.js`;
+      chatbotScript.id = chatbotScriptId;
+      chatbotScript.async = true;
+      chatbotScript.defer = true;
+      chatbotScript.onload = () => {
+        setTimeout(() => {
+          window.HubSpotConversations?.widget.load({
+            chatflowId: 244224,
+            portalId: 6187835,
+          });
+        }, 2000); // Add a 2-second delay
+      };
+      document.body.appendChild(chatbotScript);
+    }
+  }, []);
+
   return (
     <html lang="en">
       <body
